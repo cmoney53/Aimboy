bce3b1aecb255b8f1d46d77eb0e3cffc3a419ef2
cmoney53 <fastkid900@gmail.com>
2026-01-02 19:34:24 -0500

    Adjust FOV clamp minimum value to 0

--- Latest.lua
+++ Latest.lua
@@ -319,7 +319,7 @@ GameFOVUp.MouseButton1Click:Connect(function()
 end)
 
 GameFOVDown.MouseButton1Click:Connect(function()
-    GAME_FOV_VAL = math.clamp(GAME_FOV_VAL - 5, 30, 120)
+    GAME_FOV_VAL = math.clamp(GAME_FOV_VAL - 5, 0, 120)
     GameFOVMain.Text = "GAME FOV: " .. GAME_FOV_VAL
 end)
 
